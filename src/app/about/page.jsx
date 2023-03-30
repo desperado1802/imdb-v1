1c0a11082a4859a02b1fdfccd2ecf95edf4fe6cc
desperado1802 <mumun_ali@hotmail.com>
2023-03-30 16:05:28 +0300

    created the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+      <p>
+        Welcome to our movie database site! We are a platform dedicated to
+        providing movie enthusiasts with an easy-to-use and comprehensive
+        database of movies from all over the world.
+      </p>
+      <p>
+        Our goal is to be your go-to source for all things movies. Whether
+        you're looking for information on your favorite movies or the latest
+        releases, you'll find it here. Our database is updated regularly, so you
+        can always stay up-to-date with the latest movie news and reviews.
+      </p>
+      <p>
+        Our team of dedicated movie lovers work hard to ensure that our database
+        is accurate and complete. We understand that there are many different
+        movie genres and sub-genres out there, which is why we've made sure to
+        include a wide range of movies in our database. From classic films to
+        modern blockbusters, we've got you covered.
+      </p>
+      <p>
+        But our site is more than just a collection of movie information. We
+        also offer a community of movie lovers who are passionate about all
+        things cinema. Whether you're looking for movie recommendations or want
+        to discuss your favorite films with other like-minded people, our
+        community is here for you.
+      </p>
+      <p>
+        So whether you're a casual moviegoer or a die-hard cinephile, we hope
+        you'll find our site useful and enjoyable. Thank you for visiting, and
+        happy movie-watching!
+      </p>
+    </div>
+  );
+}

58d09489d9b277e91fb0d7cbf7d5543dd7cd14b6
desperado1802 <mumun_ali@hotmail.com>
2023-03-31 17:49:58 +0300

    added the &apos; to avoid deployment error

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -12,28 +12,29 @@ export default function About() {
       </p>
       <p>
         Our goal is to be your go-to source for all things movies. Whether
-        you're looking for information on your favorite movies or the latest
-        releases, you'll find it here. Our database is updated regularly, so you
-        can always stay up-to-date with the latest movie news and reviews.
+        you&apos;re looking for information on your favorite movies or the
+        latest releases, you&apos;ll find it here. Our database is updated
+        regularly, so you can always stay up-to-date with the latest movie news
+        and reviews.
       </p>
       <p>
         Our team of dedicated movie lovers work hard to ensure that our database
         is accurate and complete. We understand that there are many different
-        movie genres and sub-genres out there, which is why we've made sure to
-        include a wide range of movies in our database. From classic films to
-        modern blockbusters, we've got you covered.
+        movie genres and sub-genres out there, which is why we&apos;ve made sure
+        to include a wide range of movies in our database. From classic films to
+        modern blockbusters, we&apos;ve got you covered.
       </p>
       <p>
         But our site is more than just a collection of movie information. We
         also offer a community of movie lovers who are passionate about all
-        things cinema. Whether you're looking for movie recommendations or want
-        to discuss your favorite films with other like-minded people, our
+        things cinema. Whether you&apos;re looking for movie recommendations or
+        want to discuss your favorite films with other like-minded people, our
         community is here for you.
       </p>
       <p>
-        So whether you're a casual moviegoer or a die-hard cinephile, we hope
-        you'll find our site useful and enjoyable. Thank you for visiting, and
-        happy movie-watching!
+        So whether you&apos;re a casual moviegoer or a die-hard cinephile, we
+        hope you&apos;ll find our site useful and enjoyable. Thank you for
+        visiting, and happy movie-watching!
       </p>
     </div>
   );

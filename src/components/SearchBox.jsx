7a14e6c14797bf89f419e898db75b002843073ec
desperado1802 <mumun_ali@hotmail.com>
2023-03-31 18:49:36 +0300

    made the search bar more visible by adding border

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -17,19 +17,19 @@ export default function SearchBox() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex max-w-2xl mx-auto justify-between items-center px-5"
+      className="flex max-w-2xl mx-auto justify-between items-center px-5 mt-2"
     >
       <input
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Search keywords..."
-        className="w-full h-14 rounded-sm placeholder:gray-500 outline-none bg-transparent flex-1"
+        className="w-full h-10 rounded-sm placeholder:gray-500 outline-none bg-transparent flex-1 border border-amber-500 px-4"
       />
       <button
         disabled={!search.trim()}
         type="submit"
-        className="text-amber-500 disabled:text-gray-400"
+        className="text-amber-500 disabled:text-gray-400 ml-2"
       >
         Search
       </button>
